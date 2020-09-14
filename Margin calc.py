b77b5ddba22cc2c922343fb7bc11c8ae56adf03f
jcmpdx <67038272+jcmpdx@users.noreply.github.com>
2020-09-14 16:15:57 -0700

    Update Margin calc.py

--- Margin calc.py
+++ Margin calc.py
@@ -1,13 +1,10 @@
 # Elixir of Mongoose price break down
 print('Price of Mountain Silversage?')
 ms = input()
-
 print('Price of Plaguebloom?')
 pb = input()
-
 print('Price of Elixir of the Mongoose on the AH?')
 potion = float(input())
-
 final_pb = (float(pb) * 2)
 final_ms = (float(ms) * 2)
 final_cost = final_ms + final_pb + 0.04
