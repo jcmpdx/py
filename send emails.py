a78f36a6dde6a8503aa6aab874c9c4aaa459e8fe
jcmpdx <67038272+jcmpdx@users.noreply.github.com>
2020-09-14 18:50:07 -0700

    Update send emails.py
    update dict to edit names and email addresses. set app password.

--- send emails.py
+++ send emails.py
@@ -4,17 +4,34 @@ print(type(conn))
 fromEmail = 'joker91186@gmail.com'
 print(conn.ehlo())
 print(conn.starttls())
-# App Password in Gmail for the below password
-pw = 'xxx'
-print(conn.login(fromEmail, pw))
 
-conn.sendmail(fromEmail, 'joker91186@gmail.com', '''Subject: another email test
+# Needed to generate an App Password in Gmail for the below password
+pw = 'xxxxxxxxxxxxxxxxxxx'
+# Needed to generate an App Password in Gmail for the below password
 
-Hello,
+try:
+    print(conn.login(fromEmail, pw))
+except:
+    print('\n!!!Bad password!!!\n')
 
-Test message from Python smptlib.
+emails = {
+    'Bob Shmee': 'joker91186@gmail.com',
+    'Ensign Ro': 'jcm91186@gmail.com'
+}
 
-best,
-JC''')
+emailList = list(emails.values())
+emailNames = list(emails.keys())
+
+for i in range(0, (len(emailList))):
+    print(emailList[i])
+    print('Email message to: ' + emailNames[i])
+    conn.sendmail(fromEmail, emailList[i], 'Subject: Pyemail test\n\nDear ' + str(emailNames[i]) + ''',
+
+Thank you for taking part in this amazing experiment!
+
+Best,
+JC
+    ''')
+
 # disconnects from SMTP server
 conn.quit()
